762ae2b2c8a22727c4dff06e2a43ad01ad764686
hyperboid <23138363+polypoyo@users.noreply.github.com>
2024-11-22 15:13:51 -0500

    Move grazing logic

--- preview.lua
+++ preview.lua
@@ -100,30 +100,6 @@ function preview:init(mod, button)
 			self.bleedtimer = -0.4
 			orig_init(self, ...)
 		end
-		local soul_update_orig = Soul.update
-		function Soul:update(...)
-			if isActive() and opt("graze_behavior") ~= "None" then
-				for _,bullet in ipairs(Game.stage:getObjects(Bullet)) do
-					if bullet:collidesWith(self.graze_collider) then
-						if opt("graze_behavior") == "Delay" then
-							if bullet.grazed then
-								Game.battle.bleedtimer = Game.battle.bleedtimer - (0.9 * DT)
-							else
-								Game.battle.bleedtimer = Game.battle.bleedtimer - 1
-							end
-						else
-							if not bullet.grazed and self.inv_timer == 0 then
-								for i,v in ipairs(Game.battle.party) do
-									v.chara.health = math.min(v.chara:getStat("health"), v.chara.health + 5)
-									v:checkHealth()
-								end
-							end
-						end
-					end
-				end
-			end
-			soul_update_orig(self, ...)
-		end
 	end
 	local ebb = Kristal.Ebb
 	
---
+++ scripts/hooks/Soul.lua
@@ -0,0 +1,31 @@
+local Soul, super = Class(Soul)
+local function isActive()
+    return Kristal.Config["plugins/enabled_plugins"]["everybodybleeds"]
+end
+local function opt(setting)
+    return Kristal.Config["ebb/"..setting]
+end
+function Soul:update()
+    if isActive() and opt("graze_behavior") ~= "None" then
+        for _,bullet in ipairs(Game.stage:getObjects(Bullet)) do
+            if bullet:collidesWith(self.graze_collider) then
+                if opt("graze_behavior") == "Delay" then
+                    if bullet.grazed then
+                        Game.battle.bleedtimer = Game.battle.bleedtimer - (0.9 * DT)
+                    else
+                        Game.battle.bleedtimer = Game.battle.bleedtimer - 1
+                    end
+                else
+                    if not bullet.grazed and self.inv_timer == 0 then
+                        for i,v in ipairs(Game.battle.party) do
+                            v.chara.health = math.min(v.chara:getStat("health"), v.chara.health + 5)
+                            v:checkHealth()
+                        end
+                    end
+                end
+            end
+        end
+    end
+    super.update(self)
+end
+return Soul
